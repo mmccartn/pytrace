40e5be04b41091b4c5f03223bff4c02d321758df
burt <pixelrender@gmail.com>
2017-03-25 16:54:47 -0700

    restyle

--- camera.py
+++ camera.py
@@ -6,9 +6,9 @@ class Camera (object):
 
     __slots__ = ('origin', 'lower_left', 'horizontal', 'vertical')
 
-    def __init__(self, aspect, vfov, lookfrom, lookat, vup=Vec(0.0, 1.0, 0.0)):
-        theta = vfov * pi / 180.0
-        half_height = tan(theta / 2.0)
+    def __init__(self, aspect, vfov, lookfrom, lookat, vup=Vec(0, 1, 0)):
+        theta = vfov * pi / 180
+        half_height = tan(theta / 2)
         half_width = aspect * half_height
 
         self.origin = lookfrom
@@ -18,8 +18,8 @@ class Camera (object):
         v = Vec.cross(w, u)
 
         self.lower_left = self.origin - half_width * u - half_height * v - w
-        self.horizontal = 2.0 * half_width * u
-        self.vertical = 2.0 * half_height * v
+        self.horizontal = 2 * half_width * u
+        self.vertical = 2 * half_height * v
 
     def get_ray(self, s, t):
         return Ray(self.origin, self.lower_left + s*self.horizontal + t*self.vertical - self.origin)
